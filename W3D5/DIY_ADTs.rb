4f91d18aa54cf67dcf4540b704f21b170364c73f
MalachiCoberley <malachicoberley@gmail.com>
2021-09-17 08:28:18 -0700

    Create Queue class

--- W3D5/DIY_ADTs.rb
+++ W3D5/DIY_ADTs.rb
@@ -18,3 +18,25 @@ class Stack
     stack[-1]
   end
 end
+
+class Queue
+
+  attr_accessor :queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    queue.unshift(el)
+  end
+
+  def dequeue
+    queue.pop
+  end
+
+  def peek
+    queue[-1]
+  end
+
+end
