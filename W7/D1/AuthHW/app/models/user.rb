b90a88d26d0b8627dc49be185a0521bbd2284e9b
MalachiCoberley <malachicoberley@gmail.com>
2021-10-11 14:05:48 -0700

    pushing homework

--- W7/D1/AuthHW/app/models/user.rb
+++ W7/D1/AuthHW/app/models/user.rb
@@ -1,25 +1,45 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  before_validation :ensure_session_token
+  
   validates :username, :session_token, presence: true
-  validates :password_digest presence: {message: "Password can't be blank"}, lenght: { in: 8..22 }
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: { in: 8..22 }, allow_nil: true
 
-  def self.find_by_credentials
+  
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user
+      return user && user.is_valid_password?(password)
+    else
+      nil
+    end
   end
 
   def self.generate_session_token
 
   end
 
-  def reset_session_token
-
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
-
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
-  def password=
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
 
+  def is_valid_password(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
   end
 
 end
