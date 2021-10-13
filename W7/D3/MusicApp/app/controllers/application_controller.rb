5518014e8447116376ff460b357539e6ca74413f
MalachiCoberley <malachicoberley@gmail.com>
2021-10-12 20:09:21 -0700

    Add tests to music app

---
+++ W7/D3/MusicApp/app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::Base
+  skip_forgery_protection
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_user!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout_user!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_user!
+    redirect_to new_session_url if current_user.nil?
+  end
+
+  def require_no_user!
+    redirect_to bands_url if current_user
+  end
+
+end
